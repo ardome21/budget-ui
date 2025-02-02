3efb18b406186f6d4f4a4b8f8059b21f33e144da
ardome21 <ardome21@gmail.com>
2025-02-01 22:13:46 -0500

    make table pretty

--- src/app/transactions/presenters/transactions-table/transactions-table.component.ts
+++ src/app/transactions/presenters/transactions-table/transactions-table.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-transactions-table',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, MatTableModule ],
   templateUrl: './transactions-table.component.html',
   styleUrl: './transactions-table.component.scss'
 })
 export class TransactionsTableComponent {
-
   @Input()
   transactionData: any[] = []
+
+  displayedColumns: string[] = ['date', 'description', 'amount'];
 }
