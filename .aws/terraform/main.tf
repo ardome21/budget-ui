82cf18d4f1583cb53f9089d3fd75283b0226483b
ardome21 <ardome21@gmail.com>
2025-08-09 13:28:51 -0400

    remove duplicate

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -106,34 +106,6 @@ data "aws_iam_policy_document" "bucket_policy" {
 data "aws_caller_identity" "current" {}
 
 
-###
-
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    sid    = "AllowCloudFrontRead"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.site.arn}/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.cdn.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.site.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
-}
-
 #######################
 # CloudFront Distribution
 #######################
